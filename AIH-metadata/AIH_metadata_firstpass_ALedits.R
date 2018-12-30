1724008f387f0d45770a5503fb9d578779e659e9
Amy Lyden <amy.m.lyden@gmail.com>
2018-12-30 00:21:40 -0500

    updating scripts and adding DE gene results

--- AIH-metadata/AIH_metadata_firstpass_ALedits.R
+++ AIH-metadata/AIH_metadata_firstpass_ALedits.R
@@ -6,10 +6,10 @@ library("magrittr")
 library("ggplot2")
 
 #Read in comprehensive metadata file (downloaded from RedCap)
-AIH_metadata <- read.csv ("AASLDAutoimmunePilot_DATA_2018-10-15_0903.csv", stringsAsFactors = FALSE)
+AIH_metadata <- read.csv ("AASLDAutoimmunePilot_DATA_2018-12-17_1525.csv", stringsAsFactors = FALSE)
 
 #Filter for selected columns
-AIH_metadata_firstpass <- dplyr::select(AIH_metadata, aasld_id, ind_id, du,spl_plate, dt_isl,  case_hl_du, date_lt, date_coll, sex, race, ethn, aih_type, te_coll, age_coll,  on_tx, diff_treat, igg_coll, ast_coll, alt_coll,  alp_coll, tbili_coll, response, relapse, decomp, lt, liver_death, age_hc, ast_hc, alt_hc, alkphos_hc, tbili_hc)
+AIH_metadata_firstpass <- dplyr::select(AIH_metadata, aasld_id, ind_id, du,spl_plate, dt_isl,  case_hl_du, date_lt, date_coll, sex, race, ethn, aih_type, te_coll, age_coll,  on_tx, diff_treat, igg_coll, ast_coll, alt_coll,  alp_coll, tbili_coll, response, relapse, decomp, lt, liver_death, age_hc, ast_hc, alt_hc, alkphos_hc, tbili_hc, bx_bl_coll)
 colnames(AIH_metadata_firstpass)
 AIH_metadata_firstpass$response
 
@@ -24,7 +24,7 @@ for (i in 1:nrow(AIH_metadata_firstpass)){
 
 #Add in data regarding Fib4 and F03_F4
 ##Requires having run the algorithm data separately (code seperately; uploaded from prior file)
-AIH_metadata_QF4_join <- read.csv("18_1005_AIH_cases_QF4_calls.csv", stringsAsFactors = FALSE)
+AIH_metadata_QF4_join <- read.csv("18_1219_AIH_cases_QF4_calls.csv", stringsAsFactors = FALSE)
 AIH_metadata_firstpass <- full_join(AIH_metadata_firstpass, AIH_metadata_QF4_join, "aasld_id")
 colnames(AIH_metadata_firstpass)
 AIH_metadata_firstpass$F03_F4
@@ -156,101 +156,101 @@ AIH_metadata_firstpass$pegivirus <- ifelse (grepl ("AASLD-095|AASLD-037|AASLD-01
 
 #Writing to a data file
 setwd("../AIH-metadata/")
-write.csv (AIH_metadata_firstpass, file = "AIH_metadata_firstpass_ALedits_121018.csv", row.names = FALSE)
+write.csv (AIH_metadata_firstpass, file = "AIH_metadata_firstpass_ALedits_122018.csv", row.names = FALSE)
 
 
 #################Amy runs up to here and cuts metadata in DESeq2 file.
-
-#Making factors from cont variable from sample colleciton (ast, alt, tbili, alkp, igg)
-
-AIH_metadata_firstpass_cut <- read.csv ("AIH_metadata_firstpass_ALedits_121018.csv")
-AIH_metadata_firstpass_cut$aasld_id <- as.character(AIH_metadata_firstpass_cut$aasld_id)
-AIH_metadata_firstpass_cut$response <- as.factor(AIH_metadata_firstpass_cut$response)
-
-
-#Some summary statistics using dplyr to group and summarize data (mean, sd, iqr to assess break points)
-cutpoints_ast <- AIH_metadata_firstpass_cut %>% group_by (case_hl_du) %>% summarize (mean_ast = mean (ast), median_ast = median (ast), iqr_ast = IQR (ast))
-cutpoints_alt <- AIH_metadata_firstpass_cut %>% group_by (case_hl_du) %>% summarize (mean_alt = mean (alt), median_alt = median (alt), iqr_alt = IQR (alt))
-cutpoints_tbili <- AIH_metadata_firstpass_cut %>% group_by (case_hl_du) %>% summarize (mean_tbili = mean (bili), median_tbili = median (bili), iqr_tbili = IQR (bili))
-cutpoints_alkp <- AIH_metadata_firstpass_cut %>% group_by (case_hl_du) %>% summarize (mean_alkp = mean (alkp, na.rm = TRUE), median_alkp = median (alkp, na.rm = TRUE), iqr_alkp = IQR (alkp, na.rm = TRUE))
-cutpoints_igg <- AIH_metadata_firstpass_cut %>% group_by (case_hl_du) %>% summarize (mean_igg = mean (igg_coll, na.rm = TRUE), median_igg = median (igg_coll, na.rm = TRUE), iqr_igg = IQR (igg_coll, na.rm = TRUE))
-
-
-##ast summary stats and plot data; cutting into approx 25 bins (based on stats and #of bars in graph)
-cutpoints_ast 
-
-ggplot(AIH_metadata_firstpass, aes (x = ast) )+
-  geom_histogram(binwidth = 3) + xlim(0,100) + ylim (0, 40)
-
-
-AIH_metadata_firstpass_cut$ast <- cut (AIH_metadata_firstpass_cut$ast, 25, labels = 1:25)
-
-#age summary stats cutting into approximately 20 bins (based on the numbers of bars on the graph)
-ggplot(AIH_metadata_firstpass, aes (x = age, fill = case_hl_du)) +
-  geom_histogram(binwidth = 3) 
-
-AIH_metadata_firstpass_cut$age <- cut (AIH_metadata_firstpass_cut$age, 20, labels = 1:20)
-
-
-##alt summary stats cutting into approx 30 bins (based on stats and #of bars in graph)
-cutpoints_alt 
-
-ggplot(AIH_metadata_firstpass, aes (x = alt, fill = F03_F4_final)) +
-  geom_histogram(binwidth = 3) + xlim(0,250) + ylim (0, 40) 
-
-AIH_metadata_firstpass_cut$alt <- cut (AIH_metadata_firstpass_cut$alt, 30, labels = 1:30)
-
-##tbili summary stats cutting into approx 15 bins (based on stats and #of bars in graph)
-cutpoints_tbili
-
-ggplot(AIH_metadata_firstpass, aes (x = bili, fill = F03_F4)) +
-  geom_histogram(binwidth = 0.25) 
-
-AIH_metadata_firstpass_cut$bili <- cut (AIH_metadata_firstpass_cut$bili, 15, labels = 1:15)
-
-
-##alkp summary stats cutting into approx 20 bins (based on stats and #of bars in graph)
-cutpoints_alkp
-
-ggplot(AIH_metadata_firstpass, aes (x = alkp, fill = F03_F4)) +
-  geom_histogram(binwidth = 10) 
-
-AIH_metadata_firstpass_cut$alkp <- cut (AIH_metadata_firstpass_cut$alkp, 20, labels = 1:20)
-
-##igg summary stats cutting into approx 25 bins (based on stats and #of bars in graph)
-cutpoints_igg
-
-ggplot(AIH_metadata_firstpass, aes (x = igg_coll, fill = F03_F4)) +
-  geom_histogram(binwidth = 100) 
-
-AIH_metadata_firstpass_cut$igg_coll <- cut (AIH_metadata_firstpass_cut$igg_coll, 25, labels = 1:25)
-
-str(AIH_metadata_firstpass_cut)
-
-
-##Modifying AIH metadata to add in plate numbers (plate 1 vs plate 2), pegivirus, date isolated (dt_isl) and placeholder for water control
-
-#Adding a column to indicate the date of RNA extraction (dt_isl)
-AIH_metadata_firstpass_cut$dt_isl <- AIH_metadata$dt_isl
-
-#Casting plate 1 separately 
-AIH_metadata_firstpass_cut_1to56 <- AIH_metadata_firstpass_cut[1:55,]
-
-#Adding a row for AASLD-56, the water control
-AASLD_056 <- c("AASLD_056", "control", rep("NA", 5))
-AIH_metadata_firstpass_cut_1to56 <- rbind (AIH_metadata_firstpass_cut_1to56, AASLD_056)
-
-#Adding plate data as metadata
-AIH_metadata_firstpass_cut_1to56$plate <- as.factor ("plate_1")
-
-#Casting plate 2 separately 
-AIH_metadata_firstpass_cut_57to111 <- AIH_metadata_firstpass_cut[56:110,]
-AIH_metadata_firstpass_cut_57to111$plate <- as.factor ("plate_2")
-
-#Joining data from plates 1 and 2 as well as row 56
-AIH_metadata_firstpass_cut <- rbind (AIH_metadata_firstpass_cut_1to56, AIH_metadata_firstpass_cut_57to111)
-
-
-#Writing AASLD first pass cut
-write.csv (AIH_metadata_firstpass_cut, file = "AIH_metadata_firstpass_cut.csv", row.names = FALSE)
-
+# 
+# #Making factors from cont variable from sample colleciton (ast, alt, tbili, alkp, igg)
+# 
+# AIH_metadata_firstpass_cut <- read.csv ("AIH_metadata_firstpass_ALedits_121018.csv")
+# AIH_metadata_firstpass_cut$aasld_id <- as.character(AIH_metadata_firstpass_cut$aasld_id)
+# AIH_metadata_firstpass_cut$response <- as.factor(AIH_metadata_firstpass_cut$response)
+# 
+# 
+# #Some summary statistics using dplyr to group and summarize data (mean, sd, iqr to assess break points)
+# cutpoints_ast <- AIH_metadata_firstpass_cut %>% group_by (case_hl_du) %>% summarize (mean_ast = mean (ast), median_ast = median (ast), iqr_ast = IQR (ast))
+# cutpoints_alt <- AIH_metadata_firstpass_cut %>% group_by (case_hl_du) %>% summarize (mean_alt = mean (alt), median_alt = median (alt), iqr_alt = IQR (alt))
+# cutpoints_tbili <- AIH_metadata_firstpass_cut %>% group_by (case_hl_du) %>% summarize (mean_tbili = mean (bili), median_tbili = median (bili), iqr_tbili = IQR (bili))
+# cutpoints_alkp <- AIH_metadata_firstpass_cut %>% group_by (case_hl_du) %>% summarize (mean_alkp = mean (alkp, na.rm = TRUE), median_alkp = median (alkp, na.rm = TRUE), iqr_alkp = IQR (alkp, na.rm = TRUE))
+# cutpoints_igg <- AIH_metadata_firstpass_cut %>% group_by (case_hl_du) %>% summarize (mean_igg = mean (igg_coll, na.rm = TRUE), median_igg = median (igg_coll, na.rm = TRUE), iqr_igg = IQR (igg_coll, na.rm = TRUE))
+# 
+# 
+# ##ast summary stats and plot data; cutting into approx 25 bins (based on stats and #of bars in graph)
+# cutpoints_ast 
+# 
+# ggplot(AIH_metadata_firstpass, aes (x = ast) )+
+#   geom_histogram(binwidth = 3) + xlim(0,100) + ylim (0, 40)
+# 
+# 
+# AIH_metadata_firstpass_cut$ast <- cut (AIH_metadata_firstpass_cut$ast, 25, labels = 1:25)
+# 
+# #age summary stats cutting into approximately 20 bins (based on the numbers of bars on the graph)
+# ggplot(AIH_metadata_firstpass, aes (x = age, fill = case_hl_du)) +
+#   geom_histogram(binwidth = 3) 
+# 
+# AIH_metadata_firstpass_cut$age <- cut (AIH_metadata_firstpass_cut$age, 20, labels = 1:20)
+# 
+# 
+# ##alt summary stats cutting into approx 30 bins (based on stats and #of bars in graph)
+# cutpoints_alt 
+# 
+# ggplot(AIH_metadata_firstpass, aes (x = alt, fill = F03_F4_final)) +
+#   geom_histogram(binwidth = 3) + xlim(0,250) + ylim (0, 40) 
+# 
+# AIH_metadata_firstpass_cut$alt <- cut (AIH_metadata_firstpass_cut$alt, 30, labels = 1:30)
+# 
+# ##tbili summary stats cutting into approx 15 bins (based on stats and #of bars in graph)
+# cutpoints_tbili
+# 
+# ggplot(AIH_metadata_firstpass, aes (x = bili, fill = F03_F4)) +
+#   geom_histogram(binwidth = 0.25) 
+# 
+# AIH_metadata_firstpass_cut$bili <- cut (AIH_metadata_firstpass_cut$bili, 15, labels = 1:15)
+# 
+# 
+# ##alkp summary stats cutting into approx 20 bins (based on stats and #of bars in graph)
+# cutpoints_alkp
+# 
+# ggplot(AIH_metadata_firstpass, aes (x = alkp, fill = F03_F4)) +
+#   geom_histogram(binwidth = 10) 
+# 
+# AIH_metadata_firstpass_cut$alkp <- cut (AIH_metadata_firstpass_cut$alkp, 20, labels = 1:20)
+# 
+# ##igg summary stats cutting into approx 25 bins (based on stats and #of bars in graph)
+# cutpoints_igg
+# 
+# ggplot(AIH_metadata_firstpass, aes (x = igg_coll, fill = F03_F4)) +
+#   geom_histogram(binwidth = 100) 
+# 
+# AIH_metadata_firstpass_cut$igg_coll <- cut (AIH_metadata_firstpass_cut$igg_coll, 25, labels = 1:25)
+# 
+# str(AIH_metadata_firstpass_cut)
+# 
+# 
+# ##Modifying AIH metadata to add in plate numbers (plate 1 vs plate 2), pegivirus, date isolated (dt_isl) and placeholder for water control
+# 
+# #Adding a column to indicate the date of RNA extraction (dt_isl)
+# AIH_metadata_firstpass_cut$dt_isl <- AIH_metadata$dt_isl
+# 
+# #Casting plate 1 separately 
+# AIH_metadata_firstpass_cut_1to56 <- AIH_metadata_firstpass_cut[1:55,]
+# 
+# #Adding a row for AASLD-56, the water control
+# AASLD_056 <- c("AASLD_056", "control", rep("NA", 5))
+# AIH_metadata_firstpass_cut_1to56 <- rbind (AIH_metadata_firstpass_cut_1to56, AASLD_056)
+# 
+# #Adding plate data as metadata
+# AIH_metadata_firstpass_cut_1to56$plate <- as.factor ("plate_1")
+# 
+# #Casting plate 2 separately 
+# AIH_metadata_firstpass_cut_57to111 <- AIH_metadata_firstpass_cut[56:110,]
+# AIH_metadata_firstpass_cut_57to111$plate <- as.factor ("plate_2")
+# 
+# #Joining data from plates 1 and 2 as well as row 56
+# AIH_metadata_firstpass_cut <- rbind (AIH_metadata_firstpass_cut_1to56, AIH_metadata_firstpass_cut_57to111)
+# 
+# 
+# #Writing AASLD first pass cut
+# write.csv (AIH_metadata_firstpass_cut, file = "AIH_metadata_firstpass_cut.csv", row.names = FALSE)
+# 
--- AIH-metadata/Algorithm_F0toF3_vs_F4_determination.R
+++ AIH-metadata/Algorithm_F0toF3_vs_F4_determination.R
@@ -5,34 +5,35 @@
 
 #Step 1) Set working directory AIH folder, R projects, project name Determine_if_cirrhotic, and rename columns
 #setwd("~/Documents/AIH/AIH_R_Projects/Determine_if_cirrhotic")
-AIH_cases_QF4 <- read.csv(file = "AASLDAutoimmunePilot_DATA_2018-12-14_1535.csv", stringsAsFactors = FALSE)
+AIH_cases_QF4 <- read.csv(file = "AASLDAutoimmunePilot_DATA_2018-12-17_1525.csv", stringsAsFactors = FALSE)
 colnames(AIH_cases_QF4)[which(colnames(AIH_cases_QF4)=="ast_coll")] <- "AST"
 colnames(AIH_cases_QF4)[which(colnames(AIH_cases_QF4)=="alt_coll")] <- "ALT"
 colnames(AIH_cases_QF4)[which(colnames(AIH_cases_QF4)=="plt_coll")] <- "Plts"
 colnames(AIH_cases_QF4)[which(colnames(AIH_cases_QF4)=="te_coll")] <- "fs_SC"
-#colnames(AIH_cases_QF4)[which(colnames(AIH_cases_QF4)=="bx_fib_coll")] <- "Bx_SC"
+colnames(AIH_cases_QF4)[which(colnames(AIH_cases_QF4)=="bx_fib_coll")] <- "Bx_SC"
 colnames(AIH_cases_QF4)[which(colnames(AIH_cases_QF4)=="ov_fib_coll")] <- "Overall_fibrosis_review"
 colnames(AIH_cases_QF4)[which(colnames(AIH_cases_QF4)=="bx_fib_bl")] <- "Bx_BL"
 
 colnames(AIH_cases_QF4)[which(colnames(AIH_cases_QF4)=="te_bl")] <- "fs_BL"
 colnames(AIH_cases_QF4)[which(colnames(AIH_cases_QF4)=="age_coll")] <- "age"
 
-AIH_cases_QF4
+
 
 #Step 2) add baseline bx and Fib4 data, creating Fib4 algorithm, setting bx_bl_coll as the Bx6m (did they get a biopsy within six months of sample collection?), calculate the fib4 score
 Fib4 <- function (Age, AST, Plts, ALT) {
   ((Age)*(AST))/((Plts)*(sqrt(ALT)))
 }
-AIH_cases_QF4$bx_bl_coll
+#AIH_cases_QF4$bx_bl_coll
 #BLbx6m <- read.csv(file = "./Data/AASLD_cases_BLvsSC_dates.csv")
 #AIH_cases_QF4$Bx6m <- BLbx6m [ ,1]
 AIH_cases_QF4$Bx6m <- AIH_cases_QF4$bx_bl_coll
 AIH_cases_QF4$fib4 <- Fib4 (AIH_cases_QF4$age, AIH_cases_QF4$AST, AIH_cases_QF4$Plts, AIH_cases_QF4$ALT)
 
 #Step 3) For selection - recasting Stage "Stage 1" as 1 and then making biopsy numberic
-AIH_cases_QF4 [8,7] = 1
+#AIH_cases_QF4 [8,7] = 1
 
 AIH_cases_QF4$Bx_SC <- as.integer(as.character(AIH_cases_QF4$Bx_SC))
+AIH_cases_QF4$Bx_SC
 
 #Step 4) Selecting on baseline biopsy 1st and if not avail, biopsy w/in 6 mos,then fibrosacn at sample collection (>= 19 or < 13); 
 #for fibrosacan at sample collection, if none of the former data was avaialbe and the fibroscan score was >=13 or < 19, 
@@ -62,12 +63,13 @@ library(dplyr)
 library(stringr)
 library(magrittr)
 
-overall_fibrosis_review <- read.csv ("AASLDAutoimmunePilot_DATA_2018-10-15_0903.csv")
+overall_fibrosis_review <- read.csv ("AASLDAutoimmunePilot_DATA_2018-12-17_1525.csv")
 
 #Select just the cases for casting overally fibrosis
 ofr_cases <- filter(overall_fibrosis_review, case_hl_du == 1)
 ofr_cases <- select(ofr_cases, aasld_id, ov_fib_coll)
 AIH_cases_QF4_filter <- filter(AIH_cases_QF4, case_hl_du == 1)
+ofr_cases
 
 #merge overall fibrosis review column (for cases) in master QF4 data with updated info from redcap red in above and then replace with new data
 AIH_cases_QF4_filter$ofr <- ofr_cases$ov_fib_col
@@ -84,7 +86,7 @@ AIH_cases_QF4_filter$ofr <- ifelse (str_detect(AIH_cases_QF4_filter$ofr, 'CL'), 
 AIH_cases_QF4_filter$ofr <- as.numeric(AIH_cases_QF4_filter$ofr)
 
 ##Casting overall fibrosis review ofr column into the calls
-AIH_cases_QF4_filter
+#AIH_cases_QF4_filter
 
 AIH_cases_QF4_filter$F03_F4 <- ifelse (
                          AIH_cases_QF4_filter$F03vF4 %in% "F0_F3", print ("no"),
@@ -103,7 +105,7 @@ AIH_cases_QF4_calls <- data.frame (aasld_id = AIH_cases_QF4_filter$aasld_id, fib
 
 #Write files for import into main metadata file
 #write.csv(AIH_cases_QF4, file = "18_1005_AIH_metadata.csv", row.names = FALSE)
-write.csv(AIH_cases_QF4_calls, file = "18_1214_AIH_cases_QF4_calls.csv", row.names = FALSE)
+write.csv(AIH_cases_QF4_calls, file = "18_1219_AIH_cases_QF4_calls.csv", row.names = FALSE)
 
 
 
