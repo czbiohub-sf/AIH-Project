7b84e2d41e3f1fd70273b8df0c3f5c09d969ca71
Amy Lyden <amy.m.lyden@gmail.com>
2019-01-29 21:36:15 -0800

    updating metadata and fibrosis algorithm scripts, adding fibrosis DE results

--- AIH-metadata/Algorithm_F0toF3_vs_F4_determination.R
+++ AIH-metadata/Algorithm_F0toF3_vs_F4_determination.R
@@ -5,7 +5,7 @@
 
 #Step 1) Set working directory AIH folder, R projects, project name Determine_if_cirrhotic, and rename columns
 #setwd("~/Documents/AIH/AIH_R_Projects/Determine_if_cirrhotic")
-AIH_cases_QF4 <- read.csv(file = "AASLDAutoimmunePilot_DATA_2018-12-17_1525.csv", stringsAsFactors = FALSE)
+AIH_cases_QF4 <- read.csv(file = "../github/AIH-Project/AIH-metadata/AASLDAutoimmunePilot_DATA_2019-01-29_1413.csv", stringsAsFactors = FALSE)
 colnames(AIH_cases_QF4)[which(colnames(AIH_cases_QF4)=="ast_coll")] <- "AST"
 colnames(AIH_cases_QF4)[which(colnames(AIH_cases_QF4)=="alt_coll")] <- "ALT"
 colnames(AIH_cases_QF4)[which(colnames(AIH_cases_QF4)=="plt_coll")] <- "Plts"
@@ -23,17 +23,18 @@ colnames(AIH_cases_QF4)[which(colnames(AIH_cases_QF4)=="age_coll")] <- "age"
 Fib4 <- function (Age, AST, Plts, ALT) {
   ((Age)*(AST))/((Plts)*(sqrt(ALT)))
 }
-#AIH_cases_QF4$bx_bl_coll
-#BLbx6m <- read.csv(file = "./Data/AASLD_cases_BLvsSC_dates.csv")
-#AIH_cases_QF4$Bx6m <- BLbx6m [ ,1]
-AIH_cases_QF4$Bx6m <- AIH_cases_QF4$bx_bl_coll
+
+AIH_cases_QF4$Bx6m <- ifelse(AIH_cases_QF4$bx_bl_coll == 1, "yes",
+                             ifelse(AIH_cases_QF4$bx_bl_coll == 0, "no", NA))
 AIH_cases_QF4$fib4 <- Fib4 (AIH_cases_QF4$age, AIH_cases_QF4$AST, AIH_cases_QF4$Plts, AIH_cases_QF4$ALT)
 
-#Step 3) For selection - recasting Stage "Stage 1" as 1 and then making biopsy numberic
-#AIH_cases_QF4 [8,7] = 1
 
+
+#Step 3) For selection - recasting Stage "Stage 1" as 1 and then making biopsy numberic
+#AIH_cases_QF4 [8,7] = 1 (this is already true)
 AIH_cases_QF4$Bx_SC <- as.integer(as.character(AIH_cases_QF4$Bx_SC))
-AIH_cases_QF4$Bx_SC
+
+
 
 #Step 4) Selecting on baseline biopsy 1st and if not avail, biopsy w/in 6 mos,then fibrosacn at sample collection (>= 19 or < 13); 
 #for fibrosacan at sample collection, if none of the former data was avaialbe and the fibroscan score was >=13 or < 19, 
@@ -58,28 +59,25 @@ AIH_cases_QF4$Fib4Call <- ifelse(
   AIH_cases_QF4$fib4 < 2.6 & !is.na(AIH_cases_QF4$fib4), print ("F0_F3"), NA)
 
 
-#Step 5a) read in updated overall fibrosis review data from "all data" downlod in redcap and then swap in new overall fibrosis review data
+#Step 5) read in updated overall fibrosis review data from "all data" downlod in redcap and then swap in new overall fibrosis review data
 library(dplyr)
 library(stringr)
 library(magrittr)
 
-overall_fibrosis_review <- read.csv ("AASLDAutoimmunePilot_DATA_2018-12-17_1525.csv")
+overall_fibrosis_review <- read.csv ("../github/AIH-Project/AIH-metadata/AASLDAutoimmunePilot_DATA_2019-01-29_1413.csv")
 
 #Select just the cases for casting overally fibrosis
 ofr_cases <- filter(overall_fibrosis_review, case_hl_du == 1)
 ofr_cases <- select(ofr_cases, aasld_id, ov_fib_coll)
 AIH_cases_QF4_filter <- filter(AIH_cases_QF4, case_hl_du == 1)
-ofr_cases
 
 #merge overall fibrosis review column (for cases) in master QF4 data with updated info from redcap red in above and then replace with new data
 AIH_cases_QF4_filter$ofr <- ofr_cases$ov_fib_col
 AIH_cases_QF4_filter$ofr_id_match <- ofr_cases$aasld_id
 all(AIH_cases_QF4_filter$aasld_id == AIH_cases_QF4$ofr_id_match)
-AIH_cases_QF4_filter$ofr
 
 #Case CL calls as character, select columns that "start with" CL; update columns with CL to be numeric values
 AIH_cases_QF4_filter$ofr <- as.character(AIH_cases_QF4_filter$ofr)
-ofr_reassign <- AIH_cases_QF4_filter %>% filter (str_detect(ofr, 'CL')) %>% mutate(ofr = str_sub(ofr, 12))
 
 #Reassigning ofr column wtih ofr reassign as needed and then casting as integer
 AIH_cases_QF4_filter$ofr <- ifelse (str_detect(AIH_cases_QF4_filter$ofr, 'CL'), str_sub(AIH_cases_QF4_filter$ofr, 12), AIH_cases_QF4_filter$ofr)
@@ -105,7 +103,7 @@ AIH_cases_QF4_calls <- data.frame (aasld_id = AIH_cases_QF4_filter$aasld_id, fib
 
 #Write files for import into main metadata file
 #write.csv(AIH_cases_QF4, file = "18_1005_AIH_metadata.csv", row.names = FALSE)
-write.csv(AIH_cases_QF4_calls, file = "18_1219_AIH_cases_QF4_calls.csv", row.names = FALSE)
+write.csv(AIH_cases_QF4_calls, file = "19_0129_AIH_cases_QF4_calls.csv", row.names = FALSE)
 
 
 
