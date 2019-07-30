6ef6bd6010d57b7a3fc47d38054fe6cf54058a6b
Amy Lyden <amy.m.lyden@gmail.com>
2019-07-30 16:54:27 -0700

    updating AIH DESeq script with new deidentified metadata path

---
+++ AIH-NovaSeq-Gene-Counts/AIHReseq/generate_gene_counts.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for i in `ls *1302019*`; do cut -f2 $i > $i.o; done; #take the second column in tab file and make a new file with .o extension
+paste `ls *.o` > all_counts.tsv #paste together (column after column) all these new files with the second column only
+cut -f1 `ls *1302019* | head -1` > genenames.txt #take the first column of one of the tab files and make a gene names file
+paste genenames.txt all_counts.tsv > gene_counts.tsv #make a new file with gene names, followed by the counts from each sample
+ls *1302019*.o > filenames.txt #make a txt file with all the filenames
+tr "\n" "\t" < filenames.txt > filenames2.txt #pipe all the filenames.txt file into the tr command, which will replace enter with tab
+cat <(echo -ne '\t') filenames2.txt > filenames.txt # add tab to beginning of filenames file to columns line up
+#cat filenames.txt `echo "\n"` gene_counts.tsv > gene_counts_final.tsv
+awk '{print}' filenames.txt gene_counts.tsv > aihreseq_full_cutfilt_nodash_gene_counts_final.tsv #print out the filenames, followed by the gene counts in a row after row fashion
+
+#remove all unnecessary files
+rm *.o
+rm genenames.txt
+rm gene_counts.tsv 
+rm all_counts.tsv
+rm filenames2.txt
+
